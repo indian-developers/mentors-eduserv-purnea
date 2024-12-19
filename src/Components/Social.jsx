721bad99269510cbe9c97b0d074d497e18655138
Harsh Kumar <harshj7.net@gmail.com>
2024-12-19 19:41:54 +0530

    change socil link

--- src/Components/Social.jsx
+++ src/Components/Social.jsx
@@ -39,9 +39,9 @@ function Social() {
                     <div className='fixed bottom-96 right-4'>
                         <div className="py-2">
                             <ul className="flex flex-col">
-                                <Link to={"#"} target="_blank" className="border-2 border-txtcol my-2 bg-red p-3 bg-sec1 text-blue-600 hover:bg-gray-200 rounded-full"><li><FaFacebookF size={23} /></li></Link>
+                                <Link to={"https://www.facebook.com/profile.php?id=61570262879410"} target="_blank" className="border-2 border-txtcol my-2 bg-red p-3 bg-sec1 text-blue-600 hover:bg-gray-200 rounded-full"><li><FaFacebookF size={23} /></li></Link>
                                 <Link to={"https://www.youtube.com/@chingarimediaofficial"} target="_blank" className="border-2 border-txtcol my-2 bg-red p-3 bg-sec1 text-red-700 hover:bg-gray-200 rounded-full"><li><FaYoutube size={23} /></li></Link>
-                                <Link to={"https://www.instagram.com/chingarimedia"} target="_blank" className="border-2 border-txtcol my-2 bg-red p-3 bg-sec1 text-[#E1306C] hover:bg-gray-200 rounded-full"><li><FaInstagram size={23} /></li></Link>
+                                <Link to={"https://www.instagram.com/mentors_eduserv_purnea"} target="_blank" className="border-2 border-txtcol my-2 bg-red p-3 bg-sec1 text-[#E1306C] hover:bg-gray-200 rounded-full"><li><FaInstagram size={23} /></li></Link>
                                 <Link to={"https://www.linkedin.com/company/chingari-media"} target="_blank" className="border-2 border-txtcol my-2 bg-red p-3 bg-sec1 text-blue-500 hover:bg-gray-200 rounded-full"><li><FaLinkedin size={23} /></li></Link>
                             </ul >
                         </div >
